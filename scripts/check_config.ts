4e89a30ee4b7a9ea0be680e29143b34ee104a6e0
parallelventures <ios@parallelventures.eu>
2026-02-28 21:16:59 +0100

    fix(deploy): Remove test images and scripts from repo, update gitignore

--- scripts/check_config.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function run() {
-    const cron = await prisma.cronJob.findFirst({ where: { actionType: "GENERATE_DAILY_SETS" } });
-    console.log("Cron config:", cron?.configJson);
-}
-run();
